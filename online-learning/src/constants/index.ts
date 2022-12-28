568e37ef7b3c57fe0dbd1d96d1fb1788933c70f5
NovyAPP <micnowotnik@gmail.com>
2022-12-28 10:27:53 +0100

    boilerplate init

---
+++ online-learning/src/constants/index.ts
@@ -0,0 +1,7 @@
+import constants from "./constants";
+import theme, { COLORS, SIZES } from "./theme";
+import images from "./images";
+import icons from "./icons";
+import dummyData from "./dummyData";
+
+export { constants, theme, COLORS, SIZES, images, icons, dummyData };
