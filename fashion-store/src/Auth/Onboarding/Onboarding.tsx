582d98190d70a5e013dc46db56bd1aa28c8cd795
NovyAPP <micnowotnik@gmail.com>
2023-01-02 12:03:37 +0100

    Fonts and Asset loader boilerplate

---
+++ fashion-store/src/Auth/Onboarding/Onboarding.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Onboarding = () => {
+  return (
+    <View style={{ flex: 1, backgroundColor: "cyan" }}>
+      <Text>Onboarding</Text>
+    </View>
+  );
+};
+
+export default Onboarding;
